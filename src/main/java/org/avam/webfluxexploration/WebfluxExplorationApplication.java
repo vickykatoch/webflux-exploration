730476240a3aba48d5130b6ed86522d3551469eb
vickykatoch <vickykatoch@yahoo.com>
2022-08-30 21:06:19 -0400

    initial checkin

---
+++ src/main/java/org/avam/webfluxexploration/WebfluxExplorationApplication.java
@@ -0,0 +1,13 @@
+package org.avam.webfluxexploration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxExplorationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxExplorationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/avam/webfluxexploration/WebfluxExplorationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.avam.webfluxexploration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxExplorationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
